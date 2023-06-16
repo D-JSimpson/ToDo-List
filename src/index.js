005dd60d5674b685476db9e2d11c8e118d92437c
D-JSimpson <simpsondante03@gmail.com>
2023-06-16 19:01:07 -0400

    Change The Way Sidebar is collapsed
    Instead of inline css use toggle classes,
    So the sidebar can now be open when the page is loaded.
    
    Remove no longer needed functions related to open/close of sidebar

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const navBar = document.createElement("div");
 navBar.id = "navBar";
 const mySidebar = document.createElement("div");
 mySidebar.classList.add("sidebar");
+mySidebar.classList.add("sidebarOpen");
 
 // Projects For the SideBar
 const projectLabelContainer = document.createElement("div");
@@ -19,7 +20,7 @@ projectLabelContainer.id = "projectLabelContainer";
 const projectLabel = document.createElement("span");
 projectLabel.innerText = "Projects";
 const openClose = document.createElement("span");
-openClose.innerText = "\\x2796"; //   \02795
+openClose.innerText = "\u2795"; //   \  2796
 
 const projectsContainer = document.createElement("div");
 const projectHome = document.createElement("a");
@@ -31,28 +32,21 @@ projectEducation.innerText = "Educaition";
 // Dummy content for site
 const content = document.createElement("div");
 content.id = "content";
+content.classList.add("contentPushed");
 const h2 = document.createElement("h2");
 h2.innerText = "Collapsed Sidebar";
 const p = document.createElement("p");
 p.innerText = "Content...";
 
 /* Hamburger Switch Toggle Functions */
-function addSidebar(el) {
-  el.setAttribute("aria-checked", "false");
-  mySidebar.style.width = "250px";
-  content.style.marginLeft = "250px";
-}
-// Change To Classes Instead of Inline CSS Also solves being there on open
-function removeSideBar(el) {
-  el.setAttribute("aria-checked", "true");
-  mySidebar.style.width = "0";
-  content.style.marginLeft = "0px";
-}
+// Change To Classes Instead of Inline CSS Also solves being there on open, Sidebar not actully bellow nav
 function handleClickEvent(evt) {
   const el = evt.target;
   el.getAttribute("aria-checked") === "true"
-    ? addSidebar(el)
-    : removeSideBar(el);
+    ? el.setAttribute("aria-checked", "false")
+    : el.setAttribute("aria-checked", "true");
+  mySidebar.classList.toggle("sidebarOpen");
+  content.classList.toggle("contentPushed");
 }
 
 /*--------------------------------------------*/
@@ -67,7 +61,7 @@ sidebarHamburger.setAttribute("type", "button");
 sidebarHamburger.setAttribute("role", "switch");
 sidebarHamburger.setAttribute("aria-checked", "true");
 sidebarHamburger.addEventListener("click", handleClickEvent, false);
-sidebarHamburger.addEventListener("click", handleClickEvent, false);
+//sidebarHamburger.addEventListener("click", handleClickEvent, false);
 
 /* Append Children */
 body.append(navBar, mySidebar, content);
